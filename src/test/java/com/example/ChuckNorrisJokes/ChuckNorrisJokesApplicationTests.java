f100473ada5d8e46824a64da2276560a58de79e0
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-07-13 13:14:05 +0200

    Initialization

---
+++ src/test/java/com/example/ChuckNorrisJokes/ChuckNorrisJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChuckNorrisJokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
