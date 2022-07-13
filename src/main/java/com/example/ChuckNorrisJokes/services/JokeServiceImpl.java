1b03e53bbe8a528ecc91ec6933d1fecc3722d3a2
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-07-13 16:26:34 +0200

    Added Thymeleaf template to show jokes. Closes #5

--- src/main/java/com/example/ChuckNorrisJokes/services/JokeServiceImpl.java
+++ src/main/java/com/example/ChuckNorrisJokes/services/JokeServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.ChuckNorrisJokes.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
--- src/main/java/controllers/JokeController.java
+++ src/main/java/controllers/JokeController.java
@@ -3,7 +3,9 @@ package controllers;
 import com.example.ChuckNorrisJokes.services.JokeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @Controller
 public class JokeController {
@@ -14,7 +16,7 @@ public class JokeController {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping({"/",""})
+    @GetMapping({"/", ""})
     public String showJoke(Model model)
     {
         model.addAttribute("joke",jokeService.getJoke());
